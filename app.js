59767d5be217ae34702847bce5b2924bf71075b9
Alex <aleksandar.lokoski@gmail.com>
2021-01-09 01:14:34 -0500

    End of week 2

--- app.js
+++ app.js
@@ -12,6 +12,20 @@ const promotionRouter = require('./routes/promotionRouter')
 
 var app = express();
 
+const mongoose = require('mongoose');
+
+const url = 'mongodb://localhost:27017/nucampsite';
+const connect = mongoose.connect(url, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+});
+
+connect.then(() => console.log('Connected correctly to server'), 
+    err => console.log(err)
+);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -24,7 +38,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/campsite', campsiteRouter);
+app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
 // catch 404 and forward to error handler
